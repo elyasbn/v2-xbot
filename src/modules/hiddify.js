d1f76ba90f8061d7f3d9275b1d1eb0c35c0309b2
elyasbn <elyasbn@gmail.com>
2024-12-14 18:55:52 +0330

    Update hiddify.js

--- src/modules/hiddify.js
+++ src/modules/hiddify.js
@@ -1,68 +1,86 @@
-"use strict";
-
-const {v4: uuidv4} = require('uuid');
-
-
+const { v4: uuidv4 } = require("uuid");
 const wFetch = require("./wfetch");
 
-
 module.exports = class Hiddify {
-    baseUrl = "https://v2proxy.rew0rk.online/";
+    baseUrl = "https://serhat.marcman.eu/{proxy_path}/api/v2/admin/user/";
 
-    constructor() {
+    constructor(apiKey) {
+        this.apiKey = apiKey;
     }
 
     async createAccount(plan, server, userChatId, comment = "", options = {}) {
-        let url = new URL("/hiddify/create", this.baseUrl);
+        const url = this.baseUrl;
+        const body = {
+            added_by_uuid: null,
+            comment: comment || null,
+            current_usage_GB: 0,
+            ed25519_private_key: options.ed25519_private_key || "string",
+            ed25519_public_key: options.ed25519_public_key || "string",
+            enable: true,
+            is_active: true,
+            lang: "en",
+            last_online: null,
+            last_reset_time: null,
+            mode: "no_reset",
+            name: options.customName || `${server.remark}-${userChatId}-${Date.now()}`,
+            package_days: plan.maxDays || 0,
+            start_date: new Date().toISOString().split("T")[0],
+            telegram_id: userChatId,
+            usage_limit_GB: plan.volume || 0,
+            uuid: options.uuid || uuidv4(),
+            wg_pk: options.wg_pk || "string",
+            wg_psk: options.wg_psk || "string",
+            wg_pub: options.wg_pub || "string"
+        };
 
-        let raw = {
-            // uuid: uuidv4(),
-            "baseUrl": server.url,
-            "name": options.customName || `${server.remark}-${userChatId}-${new Date().toUnixTIme()}`,
-            "volume": plan.volume,
-            "maxDays": plan.maxDays,
-            "telegramId": userChatId,
-            "comment": comment
+        return this.sendRequest(url, 'POST', body);
+    }
+
+    async extendAccount(plan, server, userChatId, uuid, options = {}) {
+        const url = this.baseUrl + `${uuid}/extend`;
+        const body = {
+            added_by_uuid: null,
+            comment: options.comment || null,
+            current_usage_GB: 0,
+            ed25519_private_key: options.ed25519_private_key || "string",
+            ed25519_public_key: options.ed25519_public_key || "string",
+            enable: true,
+            is_active: true,
+            lang: "en",
+            last_online: null,
+            last_reset_time: null,
+            mode: "no_reset",
+            name: options.customName || `${server.remark}-${userChatId}-${Date.now()}`,
+            package_days: plan.maxDays || 0,
+            start_date: new Date().toISOString().split("T")[0],
+            telegram_id: userChatId,
+            usage_limit_GB: plan.volume || 0,
+            uuid: uuid,
+            wg_pk: options.wg_pk || "string",
+            wg_psk: options.wg_psk || "string",
+            wg_pub: options.wg_pub || "string"
+        };
+
+        return this.sendRequest(url, 'POST', body);
+    }
+
+    async sendRequest(url, method, body) {
+        const options = {
+            method: method,
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Hiddify-API-Key': this.apiKey
+            },
+            body: JSON.stringify(body)
+        };
+
+        try {
+            const response = await wFetch(url, options);
+            return await response.json();
+        } catch (error) {
+            console.error('Error during request:', error);
+            throw error;
         }
-
-        return new wFetch().request(url, 'POST', raw, {"Content-Type": "application/json"});
     }
-
-    async extendAccount(message, order, plan, server, uid, options = {}) {
-        let url = new URL(`/hiddify/extend`, this.baseUrl);
-
-        //TODO: fixme
-        let data = {
-            "baseUrl": server.url,
-            "uuid": uuidv4(),
-            "name": order.accountName,
-            "usage_limit_GB": 201,
-            "package_days": 51,
-            "comment": null,
-            "mode": "no_reset",
-            "enable": false,
-            "telegram_id": chatdId
-        }
-
-        return new wFetch().request(url, 'POST', data, {"Content-Type": "application/json"});
-
-    }
-
-    async getAccountInfo(uid, data, options = {}) {
-        let url = new URL(`/hiddify/${uid}/details`, this.baseUrl);
-        // await options.Logger?.log(`getAccountInfo url: ${url.href}`);
-
-        return new wFetch().request(url, 'POST', data, {"Content-Type": "application/json"});
-    }
-
-    async takeBackup(data, options = {}) {
-        let url = new URL("/hiddify/backup", this.baseUrl);
-
-        let raw = {
-            "baseUrl": data.serverUrl,
-        }
-
-        return new wFetch().request(url, 'POST', raw, {"Content-Type": "application/json"});
-    }
-
 }
